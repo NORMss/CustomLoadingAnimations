0adf04162dfba1b60330b5cee0f0539940ca1594
NORMss <serega.b301@gmail.com>
2024-12-16 17:14:06 +0700

    feat: created animations of pulsation and blurring of text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "ru.normno.mycustomloadinganimations"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ru.normno.mycustomloadinganimations"
